0f21b1476dc67eb482cdc0eb145126bf8cd7fdab
KPC-018A1441\HiromuChuubachi <banapekko1205@gmail.com>
2019-06-18 15:58:49 +0900

    Bossの当たり判定作成、スクリプト作成

---
+++ Assets/Scripts/Boss/BossHelth.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossHelth : MonoBehaviour
+{
+    [SerializeField] int currentHp;
+    [SerializeField] int maxHP;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHp = maxHP;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void Damage(int damage)
+    {
+        damage -= maxHP;
+    }
+}
