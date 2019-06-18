75493b73f08fd4e7282e54f8f114b1d11ef3fcf9
taku01839 <taku01839@gmail.com>
2019-06-18 15:56:04 +0900

    飛行しているEnemyの大体の完成

---
+++ Assets/Scripts/Enemy/Bullet.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    //PlayerHealthを入れる
+    public int attkDa = 10;
+    public int speed = 1;
+
+    private GameObject enemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //PlayerHelthをGetComする
+        enemy = GameObject.FindGameObjectWithTag("Enemy");
+    }
+
+    private void Update()
+    {
+        transform.position += new Vector3(0, 0, speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            //ダメージを入れるスクリプトを
+        }
+    }
+}
--- Assets/Scripts/Enemy/EnemyMove.cs
+++ Assets/Scripts/Enemy/EnemyMove.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class EnemyMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float speed = 0.5f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += new Vector3(0, 0, -speed);
     }
 }
